37058e37d3d508d734faa034ecd1750b620a9161
avodhel <uyelik-eposta@hotmail.com>
2020-04-27 13:18:56 +0300

    gitignore updated

---
+++ App1/wwwroot/js/Cookie/cookieUI.js
@@ -0,0 +1,12 @@
+﻿
+$("#btnSetCookie").click(function () {
+    var key = $("#textKey").val();
+    var value = $("#textValue").val();
+    setCookie(key, value);
+});
+
+$("#btnGetCookie").click(function () {
+    var key = $("#textKey").val();
+    var value = getCookie(key);
+    alert(value);
+});
---
+++ App1/wwwroot/js/Students/students.js
@@ -0,0 +1,25 @@
+﻿$("#saveStudents").click(function () {
+    $.ajax({
+        url: '/ogrenci-guncelle',
+        method: 'POST',
+        dataType: 'json',
+        data: {
+            Id: $("#id").val(),
+            Name: $("#name").val(),
+            Surname: $("#surname").val(),
+            Class: $("#class").val(),
+            Teacher: $("#teacher").val()
+        },
+        success: function (data) {
+            alert(data.value);
+        },
+        error: function () {
+            alert("hata");
+        },
+        statusCode: {
+            200: function (data) {
+                alert("Http Response :  200 => " + data.value)
+            }
+        }
+    });
+});
