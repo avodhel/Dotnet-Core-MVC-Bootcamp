23fff7f690d8f2e2d5d6abe68966676d43c51a5b
avodhel <uyelik-eposta@hotmail.com>
2020-05-21 15:00:59 +0300

    register screen added

---
+++ App4/App4/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using App4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App4.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = new IdentityUser()
+            {
+                Email = model.Email,
+                UserName = model.UserName
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("Kayıt Hatası", error.Description);
+            }
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View();
+        }
+    }
+}
