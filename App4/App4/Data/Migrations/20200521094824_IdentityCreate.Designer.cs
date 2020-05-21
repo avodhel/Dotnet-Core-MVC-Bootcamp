97d655983a5ea36e917b0a4456df46f9e8832736
avodhel <uyelik-eposta@hotmail.com>
2020-05-21 13:14:31 +0300

    migration with sql database

--- App4/App4/Data/Migrations/20200521094824_IdentityCreate.Designer.cs
+++ App4/App4/Data/Migrations/20200521094824_IdentityCreate.Designer.cs
@@ -4,14 +4,16 @@ using App4.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace App4.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200521094824_IdentityCreate")]
+    partial class IdentityCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ App4/App4/Data/Migrations/20200521094824_IdentityCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App4.Data.Migrations
+{
+    public partial class IdentityCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
