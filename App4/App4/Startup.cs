395a0c856911cc51453b584177436a059be1429e
avodhel <uyelik-eposta@hotmail.com>
2020-05-21 13:15:10 +0300

    close require for confirmed account

--- App4/App4/Startup.cs
+++ App4/App4/Startup.cs
@@ -30,8 +30,10 @@ namespace App4
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
