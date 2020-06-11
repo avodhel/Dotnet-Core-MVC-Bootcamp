cd38652b7d4c5859daada0a6d46ef159c36600e8
avodhel <uyelik-eposta@hotmail.com>
2020-06-11 14:15:35 +0300

    add new role

---
+++ App4/App4/Controllers/RoleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App4.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(IdentityRole role)
+        {
+            await _roleManager.CreateAsync(role);
+            return RedirectToAction("Index", "Role");
+        }
+    }
+}
