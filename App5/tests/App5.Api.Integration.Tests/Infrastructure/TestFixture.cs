4d1c82361c42aa81f1283c83380808a857d2e7b9
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-07-23 12:52:25 +0300

    setup integration test for api project

---
+++ App5/tests/App5.Api.Integration.Tests/Infrastructure/TestFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace App5.Api.Integration.Tests.Infrastructure
+{
+    public class TestFixture : IClassFixture<TestWebApplicationFactory<Startup>>
+    {
+        public readonly TestWebApplicationFactory<Startup> _factory;
+
+        public TestFixture(TestWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+    }
+}
---
+++ App5/tests/App5.Api.Integration.Tests/Infrastructure/TestWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace App5.Api.Integration.Tests.Infrastructure
+{
+    public class TestWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return WebHost.CreateDefaultBuilder().UseStartup<TStartup>();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseKestrel()
+                .UseSolutionRelativeContentRoot("")
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+                    var path = Path.Combine(env.ContentRootPath, "tests\\App5.Api.Integration.Tests");
+
+                    config.SetBasePath(path)
+                        .AddJsonFile("appsettings.json");
+                    config.AddEnvironmentVariables();
+                });
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
