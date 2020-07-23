d0ee8e985b16565ea4c840bac6c79497410b95d8
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-07-23 12:57:37 +0300

    integration test for category

---
+++ App5/tests/App5.Api.Integration.Tests/CategoryTests.cs
@@ -0,0 +1,31 @@
+﻿using App5.Api.Integration.Tests.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace App5.Api.Integration.Tests
+{
+    public class CategoryTests : TestFixture
+    {
+        public CategoryTests(TestWebApplicationFactory<Startup> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task GetCategoriesReturnHttpSuccess()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/Category/GetAll");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
